2d13307ce2c07f8ed3e282456f0846635f8e9078
aishna05 <160373169+aishna05@users.noreply.github.com>
2025-12-10 18:48:39 +0530

    customer ui done

--- ecommerce-next/src/components/RequireSeller.tsx
+++ ecommerce-next/src/components/RequireSeller.tsx
@@ -5,17 +5,21 @@ import { ReactNode, useEffect } from "react";
 import { useAuth } from "@/lib/hooks/useAuth"; // ✅ your actual auth system
 
 export default function RequireSeller({ children }: { children: ReactNode }) {
-  const { user } = useAuth();   // ✅ NOT useSession
+  const { user , isLoading } = useAuth();   // ✅ NOT useSession
   const router = useRouter();
 
   useEffect(() => {
+    if (isLoading) return; 
     if (!user) {
       router.replace("/login");
       return;
     }
 
-    if (user.role !== "SELLER") {
-      router.replace("/"); // or /unauthorized
+    // if (user.role !== "SELLER") {
+    //   router.replace("/"); // or /unauthorized
+    // }
+    if(user.role=="SELLER"){
+      router.replace("/seller/dashboard");
     }
   }, [user, router]);
 
